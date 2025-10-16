03f1a817940de35d49b4d279fbff23c58a6cbf16
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-16 09:00:17 +0200

    Updated data.sql

--- data.sql
+++ data.sql
@@ -1,106 +1,118 @@
-CREATE TABLE ELEVE(
-   IdEleve INT,
-   NomEleve VARCHAR(50),
-   PrenomEleve VARCHAR(50),
-   DateNaissEleve DATE,
-   PRIMARY KEY(IdEleve)
-);
+Sub Create_Tables()
 
-CREATE TABLE CLASSE(
-   IdClasse INT,
-   NomClasse VARCHAR(25),
-   NiveauClasse VARCHAR(15),
-   AnneeScolaire VARCHAR(10),
-   PRIMARY KEY(IdClasse)
-);
+DoCmd.RunSQL "CREATE TABLE ELEVE(" & _
+   "IdEleve INT," & _
+   "NomEleve VARCHAR(50)," & _
+   "PrenomEleve VARCHAR(50)," & _
+   "DateNaissEleve DATE," & _
+   "PRIMARY KEY(IdEleve)" & _
+");"   
 
-CREATE TABLE MATIERE(
-   IdMatiere INT,
-   IntituleMat VARCHAR(50),
-   PRIMARY KEY(IdMatiere)
-);
+DoCmd.RunSQL "CREATE TABLE CLASSE(" & _
+   "IdClasse INT," & _
+   "NomClasse VARCHAR(25)," & _
+   "NiveauClasse VARCHAR(15)," & _
+   "AnneeScolaire VARCHAR(10)," & _
+   "PRIMARY KEY(IdClasse)" & _
+");"   
 
-CREATE TABLE NOTE(
-   IdNote INT,
-   DateNote DATE,
-   Coefficient DECIMAL(2,2),
-   IdMatiere INT NOT NULL,
-   IdEleve INT NOT NULL,
-   PRIMARY KEY(IdNote),
-   FOREIGN KEY(IdMatiere) REFERENCES MATIERE(IdMatiere),
-   FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)
-);
+DoCmd.RunSQL "CREATE TABLE MATIERE(" & _
+   "IdMatiere INT," & _
+   "IntituleMat VARCHAR(50)," & _
+   "PRIMARY KEY(IdMatiere)" & _
+");"   
 
-CREATE TABLE BULLETIN(
-   IdBulletin INT,
-   Trimestre INT,
-   Annee DATE,
-   IdEleve INT NOT NULL,
-   PRIMARY KEY(IdBulletin),
-   FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)
-);
+DoCmd.RunSQL "CREATE TABLE NOTE(" & _
+   "IdNote INT," & _
+   "DateNote DATE," & _
+   "Coefficient DECIMAL(2,2)," & _
+   "IdMatiere INT NOT NULL," & _
+   "IdEleve INT NOT NULL," & _
+   "PRIMARY KEY(IdNote)," & _
+   "FOREIGN KEY(IdMatiere) REFERENCES MATIERE(IdMatiere)," & _
+   "FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)" & _
+");"   
 
-CREATE TABLE UTILISATEUR(
-   IdUtilisateur INT,
-   NomUtil VARCHAR(50),
-   PrenomUtil VARCHAR(50),
-   DateNaiss DATE,
-   MotDePasse VARCHAR(25),
-   NumTel VARCHAR(15),
-   Mail VARCHAR(50),
-   PRIMARY KEY(IdUtilisateur)
-);
+DoCmd.RunSQL "CREATE TABLE BULLETIN(" & _
+   "IdBulletin INT," & _
+   "Trimestre INT," & _
+   "Annee DATE," & _
+   "IdEleve INT NOT NULL," & _
+   "PRIMARY KEY(IdBulletin)," & _
+   "FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)" & _
+");"   
 
-CREATE TABLE ADMINISTRATEUR(
-   IdAdmin INT,
-   IdUtilisateur INT NOT NULL,
-   PRIMARY KEY(IdAdmin),
-   UNIQUE(IdUtilisateur),
-   FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)
-);
+DoCmd.RunSQL "CREATE TABLE UTILISATEUR(" & _
+   "IdUtilisateur INT," & _
+   "NomUtil VARCHAR(50)," & _
+   "PrenomUtil VARCHAR(50)," & _
+   "DateNaiss DATE," & _
+   "MotDePasse VARCHAR(25)," & _
+   "NumTel VARCHAR(15)," & _
+   "Mail VARCHAR(50)," & _
+   "PRIMARY KEY(IdUtilisateur)" & _
+");"   
 
-CREATE TABLE INSCRIPTION(
-   IdInscription INT,
-   DateInscription DATE,
-   IdAdmin INT NOT NULL,
-   IdClasse INT NOT NULL,
-   IdEleve INT NOT NULL,
-   PRIMARY KEY(IdInscription),
-   FOREIGN KEY(IdAdmin) REFERENCES ADMINISTRATEUR(IdAdmin),
-   FOREIGN KEY(IdClasse) REFERENCES CLASSE(IdClasse),
-   FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)
-);
+DoCmd.RunSQL "CREATE TABLE ADMINISTRATEUR(" & _
+   "IdAdmin INT," & _
+   "Role VARCHAR(20)," & _
+   "IdUtilisateur INT NOT NULL," & _
+   "PRIMARY KEY(IdAdmin)," & _
+   "UNIQUE(IdUtilisateur)," & _
+   "FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)" & _
+");"   
 
-CREATE TABLE ENSEIGNANT(
-   IdEnseignant INT,
-   IdUtilisateur INT NOT NULL,
-   PRIMARY KEY(IdEnseignant),
-   UNIQUE(IdUtilisateur),
-   FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)
-);
+DoCmd.RunSQL "CREATE TABLE ENSEIGNANT(" & _
+   "IdEnseignant INT," & _
+   "Role VARCHAR(20)," & _
+   "IdUtilisateur INT NOT NULL," & _
+   "PRIMARY KEY(IdEnseignant)," & _
+   "UNIQUE(IdUtilisateur)," & _
+   "FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)" & _
+");"   
 
-CREATE TABLE RESPONSABLE(
-   IdResponsable INT,
-   IdUtilisateur INT NOT NULL,
-   PRIMARY KEY(IdResponsable),
-   UNIQUE(IdUtilisateur),
-   FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)
-);
+DoCmd.RunSQL "CREATE TABLE RESPONSABLE(" & _
+   "IdResponsable INT," & _
+   "Role VARCHAR(20)," & _
+   "IdUtilisateur INT NOT NULL," & _
+   "PRIMARY KEY(IdResponsable)," & _
+   "UNIQUE(IdUtilisateur)," & _
+   "FOREIGN KEY(IdUtilisateur) REFERENCES UTILISATEUR(IdUtilisateur)" & _
+");"   
 
-CREATE TABLE ENSEIGNE(
-   IdEleve INT,
-   IdEnseignant INT,
-   IdMatiere INT,
-   PRIMARY KEY(IdEleve, IdEnseignant, IdMatiere),
-   FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve),
-   FOREIGN KEY(IdEnseignant) REFERENCES ENSEIGNANT(IdEnseignant),
-   FOREIGN KEY(IdMatiere) REFERENCES MATIERE(IdMatiere)
-);
+DoCmd.RunSQL "CREATE TABLE ENSEIGNE(" & _
+   "IdClasse INT," & _
+   "IdEnseignant INT," & _
+   "IdMatiere INT," & _
+   "PRIMARY KEY(IdClasse, IdEnseignant, IdMatiere)," & _
+   "FOREIGN KEY(IdClasse) REFERENCES CLASSE(IdClasse)," & _
+   "FOREIGN KEY(IdEnseignant) REFERENCES ENSEIGNANT(IdEnseignant)," & _
+   "FOREIGN KEY(IdMatiere) REFERENCES MATIERE(IdMatiere)" & _
+");"   
 
-CREATE TABLE REPRESENTE(
-   IdEleve INT,
-   IdResponsable INT,
-   PRIMARY KEY(IdEleve, IdResponsable),
-   FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve),
-   FOREIGN KEY(IdResponsable) REFERENCES RESPONSABLE(IdResponsable)
-);
+DoCmd.RunSQL "CREATE TABLE REPRESENTE(" & _
+   "IdEleve INT," & _
+   "IdResponsable INT," & _
+   "PRIMARY KEY(IdEleve, IdResponsable)," & _
+   "FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)," & _
+   "FOREIGN KEY(IdResponsable) REFERENCES RESPONSABLE(IdResponsable)" & _
+");"   
+
+DoCmd.RunSQL "CREATE TABLE INSCRIRE(" & _
+   "IdEleve INT," & _
+   "IdAdmin INT," & _
+   "DateInscrip DATE," & _
+   "PRIMARY KEY(IdEleve, IdAdmin)," & _
+   "FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)," & _
+   "FOREIGN KEY(IdAdmin) REFERENCES ADMINISTRATEUR(IdAdmin)" & _
+");"   
+
+DoCmd.RunSQL "CREATE TABLE APPARTENIR(" & _
+   "IdEleve INT," & _
+   "IdClasse INT," & _
+   "PRIMARY KEY(IdEleve, IdClasse)," & _
+   "FOREIGN KEY(IdEleve) REFERENCES ELEVE(IdEleve)," & _
+   "FOREIGN KEY(IdClasse) REFERENCES CLASSE(IdClasse)" & _
+");"   
+
+End Sub
