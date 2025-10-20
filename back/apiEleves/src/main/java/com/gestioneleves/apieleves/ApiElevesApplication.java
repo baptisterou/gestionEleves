8371c07c44a940f294a4395b13640081e71242f4
Baptiste Roumanie <baptisterou@users.noreply.github.com>
2025-10-20 09:49:29 +0200

    archi packages

---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/ApiElevesApplication.java
@@ -0,0 +1,13 @@
+package com.gestioneleves.apieleves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiElevesApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(ApiElevesApplication.class, args);
+    }
+
+}
---
+++ back/apiEleves/src/test/java/com/gestioneleves/apieleves/ApiElevesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioneleves.apieleves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiElevesApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
