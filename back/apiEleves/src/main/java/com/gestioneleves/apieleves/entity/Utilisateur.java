705b590d11d31e349008dacd13fa092abeb8a601
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-22 16:52:46 +0200

    Added Utilisateur files

---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/entity/Utilisateur.java
@@ -0,0 +1,43 @@
+package com.gestioneleves.apieleves.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "utilisateur")
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idUtilisateur")
+    private Long idUtilisateur;
+    @Column(name="Nom")
+    private String nom;
+    @Column(name="Prenom")
+    private String prenom;
+    @Column(name="Email")
+    private String email;
+    @Column(name="MotDePasse")
+    private String motDePasse;
+    @Column(name="DateNaissance")
+    private String dateNaissance;
+    @Column(name="NumTel")
+    private String numTel;
+    @Enumerated(EnumType.STRING)
+    @Column(name="Role")
+    private Role role;
+
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Eleve> eleves = new ArrayList<>();
+
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Classe> classesEnseignant = new ArrayList<>();
+
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Matiere> matieresEnseignant = new ArrayList<>();
+
+}
---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/service/UtilisateurService.java
@@ -0,0 +1,36 @@
+package com.gestioneleves.apieleves.service;
+
+import com.gestioneleves.apieleves.dto.UtilisateurDto;
+import com.gestioneleves.apieleves.entity.Utilisateur;
+import com.gestioneleves.apieleves.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UtilisateurService {
+
+    @Autowired
+    private UtilisateurRepository UtilisateurRepository;
+
+    public List<Utilisateur> getAllUtilisateurs(){
+        return UtilisateurRepository.findAll();
+    }
+
+    public Utilisateur ajouterUtilisateur(Utilisateur utilisateur){
+        return UtilisateurRepository.save(utilisateur);
+    }
+
+    public void modifierUtilisateur(UtilisateurDto dto){
+
+    }
+
+    public void supprimerUtilisateur(Long id){
+
+    }
+
+    public UtilisateurDto getUtilisateurById(Long id){
+        return null;
+    }
+}
