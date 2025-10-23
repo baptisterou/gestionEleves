41df8c32f3b5c612b395c1d1002b9ba3409b9802
Baptiste Roumanie <baptisterou@users.noreply.github.com>
2025-10-23 14:38:59 +0200

    add SecurityConfig with basic authentication, update datasource URL, and clean up commented code in DTO and Mapper

---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.gestioneleves.apieleves.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain security(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
