e42339eeee14a7047661cf5b0342e16615eed85e
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-27 13:12:32 +0100

    Added UtilisateurDTO and inheritance

---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
@@ -0,0 +1,23 @@
+package com.gestioneleves.apieleves.dto;
+
+import com.gestioneleves.apieleves.entity.Role;
+
+public class UtilisateurDTO {
+    protected Long idUtilisateur;
+    protected String nom;
+    protected String prenom;
+}
+
+class UtilisateurEnseignantDTO extends UtilisateurDTO {
+
+}
+
+class UtilisateurResponsableDTO extends UtilisateurDTO {
+    protected String email;
+    protected String dateNaissance;
+    protected String numTel;
+}
+
+class UtilisateurAdminDTO extends UtilisateurResponsableDTO {
+    protected Role role;
+}
