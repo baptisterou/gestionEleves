4f7676b85d0b0d3b21a5e16da6a1eb9364a53137
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-28 21:35:23 +0100

    Fixed UtilisateurDTO attributes

--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
@@ -1,7 +1,3 @@
-package com.gestioneleves.apieleves.dto;
-
-import com.gestioneleves.apieleves.entity.Role;
-
 public class UtilisateurDTO {
     protected Long idUtilisateur;
     protected String nom;
@@ -9,15 +5,25 @@ public class UtilisateurDTO {
 }
 
 class UtilisateurEnseignantDTO extends UtilisateurDTO {
-
+    protected List<ClasseDTO> classesEnseignees;     // Toutes ses classes
+    protected List<MatiereDTO> matieresEnseignees;   // Toutes ses matières
+    protected List<EleveDTO> eleves;                 // Tous les élèves de ses classes
+    protected List<NoteDTO> notes;                   // Notes de tous ses élèves
 }
 
-class UtilisateurResponsableDTO extends UtilisateurEnseignantDTO {
+class UtilisateurResponsableDTO extends UtilisateurDTO {
     protected String email;
     protected String dateNaissance;
     protected String numTel;
+    protected EleveDTO eleve;                       // SON enfant seulement
+    protected List<NoteDTO> notesEleve;             // Notes de SON enfant
+    protected List<BulletinDTO> bulletinsEleve;     // Bulletins de SON enfant
 }
 
-class UtilisateurAdminDTO extends UtilisateurResponsableDTO {
+class UtilisateurAdminDTO extends UtilisateurDTO {
     protected Role role;
+    protected String motDePasse;
+    protected List<UtilisateurDTO> tousLesUtilisateurs;
+    protected List<EleveDTO> eleves;
+    protected List<NoteDTO> notes;
 }
