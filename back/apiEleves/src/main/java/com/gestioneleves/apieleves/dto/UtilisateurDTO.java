74010fad1fcdccbc0a87b27a785966386f642840
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-28 21:40:03 +0100

    extract user DTOs into separate files

--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
@@ -1,29 +1,7 @@
+package com.gestioneleves.apieleves.dto;
+
 public class UtilisateurDTO {
     protected Long idUtilisateur;
     protected String nom;
     protected String prenom;
-}
-
-class UtilisateurEnseignantDTO extends UtilisateurDTO {
-    protected List<ClasseDTO> classesEnseignees;     // Toutes ses classes
-    protected List<MatiereDTO> matieresEnseignees;   // Toutes ses matières
-    protected List<EleveDTO> eleves;                 // Tous les élèves de ses classes
-    protected List<NoteDTO> notes;                   // Notes de tous ses élèves
-}
-
-class UtilisateurResponsableDTO extends UtilisateurDTO {
-    protected String email;
-    protected String dateNaissance;
-    protected String numTel;
-    protected EleveDTO eleve;                       // SON enfant seulement
-    protected List<NoteDTO> notesEleve;             // Notes de SON enfant
-    protected List<BulletinDTO> bulletinsEleve;     // Bulletins de SON enfant
-}
-
-class UtilisateurAdminDTO extends UtilisateurDTO {
-    protected Role role;
-    protected String motDePasse;
-    protected List<UtilisateurDTO> tousLesUtilisateurs;
-    protected List<EleveDTO> eleves;
-    protected List<NoteDTO> notes;
 }
