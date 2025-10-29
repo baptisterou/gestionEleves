aadb6bb7e53fb71806a3695ba15bd7964ac5bd56
Jean-Baptiste <jean.baptiste.roumanie@gmail.com>
2025-10-29 16:58:28 +0100

    Enhance user management functionality with validation, security improvements, and mapper refactor. Updated entities with DTO support, added new dependencies, and enabled password encoding.

--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/UtilisateurDTO.java
@@ -1,7 +1,19 @@
 package com.gestioneleves.apieleves.dto;
 
+import com.gestioneleves.apieleves.entity.Role;
+
+import java.util.Date;
+
+/**
+ * Représentation publique d'un utilisateur à exposer par l'API.
+ * Ne contient jamais le mot de passe.
+ */
 public class UtilisateurDTO {
-    protected Long idUtilisateur;
-    protected String nom;
-    protected String prenom;
+    public Long idUtilisateur;
+    public String nom;
+    public String prenom;
+    public String email;
+    public String numTel;
+    public Date dateNaissance;
+    public Role role;
 }
--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/entity/Classe.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/entity/Classe.java
@@ -1,21 +1,28 @@
 package com.gestioneleves.apieleves.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.Date;
-
-@Data
+@Getter
+@Setter
+@ToString(exclude = {"enseignant"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "classe")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Classe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long idClasse;
 
     private String nomClasse;
