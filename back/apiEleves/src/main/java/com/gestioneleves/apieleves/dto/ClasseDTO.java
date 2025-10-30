2d2da1efbb9c1d1b3b39ecad7dfbbc8efed9ac2d
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-10-30 10:42:34 +0100

    Added NoArgsConstructor/AllArgs on ClasseDTO and MatiereDTO

--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/ClasseDTO.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/dto/ClasseDTO.java
@@ -1,8 +1,12 @@
 package com.gestioneleves.apieleves.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClasseDTO {
     private Long idClasse;
     private String nomClasse;
