5895e521bf27beb491d4c8c5ba2f560a8b9e15a1
Jean-Baptiste <jean.baptiste.roumanie@gmail.com>
2025-11-13 15:43:24 +0100

    Ajoute la gestion des enseignants dans les formulaires et listes des classes, matières et inscriptions. Refonte du filtre des années scolaires et des méthodes associées. Adaptation des endpoints pour supprimer les inscriptions avec `eleveId` et `utilisateurId`.

--- back/apiEleves/src/main/java/com/gestioneleves/apieleves/entity/Inscription.java
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/entity/Inscription.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "inscription")
@@ -24,6 +25,8 @@ public class Inscription {
 
     @Column(name = "DateInscrip")
     private LocalDate dateInscrip;
+
+    private List<String> inscriptions;
 
     @ManyToOne
     @JoinColumn(name = "id_eleve")
