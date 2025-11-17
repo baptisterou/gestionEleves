d3efc7bff7e19deec255f310542ffe31e45c4f93
Jean-Baptiste <jean.baptiste.roumanie@gmail.com>
2025-11-17 08:21:36 +0100

    Ajoute les entités `Enseignement` et `Parcours` avec leurs relations associées, ainsi que les repositories correspondants (`EnseignementRepository` et `ParcoursRepository`). Mise à jour de l'entité `Classe` pour inclure les nouvelles relations. Addition d'un contrôleur `EnseignementController`.

---
+++ back/apiEleves/src/main/java/com/gestioneleves/apieleves/repository/EnseignementRepository.java
@@ -0,0 +1,6 @@
+package com.gestioneleves.apieleves.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnseignementRepository extends JpaRepository<EnseignementRepository, Long> {
+}
